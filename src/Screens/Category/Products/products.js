f22305e3c87a985f126096049254bc26f36c9c5c
Vinicius Okaeda <viniciusokaeda@gmail.com>
2024-10-10 13:59:10 -0300

    update image

--- src/Screens/Category/Products/products.js
+++ src/Screens/Category/Products/products.js
@@ -1,49 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Dimensions } from 'react-native';
 import customData from "../../../../products.json";
 import Icon from 'react-native-vector-icons/AntDesign';
 
+const { width } = Dimensions.get('window'); // Pegando a largura da tela
+
 const ProductsByCategory = ({ route, navigation }) => {
   const { itemId } = route.params; // Obtém o parâmetro enviado
+  
+  // Array de imagens para o carousel
+  const images = [
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo1.png' },
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo2.png' },
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo3.png' },
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo4.png' },
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo5.png' },
+    { url: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/promo6.png' },
+  ];
 
-  //console.log("meu custom tem", customData.data.filter(e => e.products_category === itemId ))
+  const [currentIndex, setCurrentIndex] = useState(0); // Estado para controlar o índice da imagem atual
 
+  // Ref para o FlatList
+  const carouselRef = useRef(null);
+
+  // Função para atualizar o índice do carousel
+  const onViewableItemsChanged = useRef(({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setCurrentIndex(viewableItems[0].index);
+    }
+  }).current;
 
   const renderItem = ({ item }) => {
-    return(
+    return (
       <TouchableOpacity 
-      style={styles.itemContainer}
-      onPress={() => {
-        navigation.navigate('Detalhes do Produto', { itemId: item.products_id, categoryItemId: item.products_category })      
-      } }
+        style={styles.itemContainer}
+        onPress={() => {
+          navigation.navigate('Detalhes do Produto', { itemId: item.products_id, categoryItemId: item.products_category })      
+        }}
       >
         <View style={styles.containerImage}>
-            <Image style={styles.image} source={{
-          uri: item.products_image,
-        }}  />
-            <TouchableOpacity style={styles.containerImageFavoriteButton}>
-            <Icon name="heart" size={16} color="#628DB4" /> 
-            
-            </TouchableOpacity>
+          <Image style={styles.image} source={{ uri: item.products_image }} />
+          <TouchableOpacity style={styles.containerImageFavoriteButton}>
+            <Icon name="heart" size={16} color="#628DB4" />
+          </TouchableOpacity>
         </View>
 
         <View style={styles.containerContent}>
-            <Text style={styles.textHeader}>{item.products_name}</Text>
-            <Text style={styles.textContent}>R$: {item.products_price}</Text>
+          <Text style={styles.textHeader}>{item.products_name}</Text>
+          <Text style={styles.textContent}>R$: {item.products_price}</Text>
         </View>
-
       </TouchableOpacity>
+    );
+  };
+
+  // Função para renderizar cada item do Carousel
+  const renderCarouselItem = ({ item, index }) => {
+    return (
+      <View style={[styles.carouselItem, { width: width * 0.85, marginHorizontal: 10 }]}>
+        <Image style={styles.carouselImage} source={{ uri: item.url }} />
+      </View>
     );
   };
 
   return (
     <View style={styles.container}>
+      {/* Texto acima do carousel */}
+      <View style={styles.textAboveCarousel}>
+        <Text style={styles.carouselTitle}>As melhores condições para você!</Text>
+      </View>
+
+      {/* Carousel de imagens */}
+      <View style={styles.carouselContainer}>
+        <FlatList
+          ref={carouselRef}
+          data={images}
+          horizontal={true}
+          renderItem={renderCarouselItem}
+          keyExtractor={(item, index) => index.toString()}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }} // Espaçamento entre os itens
+          onViewableItemsChanged={onViewableItemsChanged} // Detecta o item visível
+          viewabilityConfig={{ itemVisiblePercentThreshold: 50 }} // Definição de visibilidade do item (50% da imagem visível)
+        />
+        {/* Indicador de página */}
+        <View style={styles.indicatorContainer}>
+          {images.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                index === currentIndex && { backgroundColor: '#628DB4', width: 10 },
+              ]}
+            />
+          ))}
+        </View>
+      </View>
+
+      {/* Imagem abaixo do carousel */}
+      <View style={styles.bottomImageContainer}>
+        <Image
+          source={{ uri: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/sale_img.png' }} // Insira o URL da imagem desejada
+          style={styles.bottomImage}
+        />
+      </View>
+
+      {/* Lista de Produtos */}
       <FlatList
-        data={customData.data.filter(e => e.products_category === itemId )}
+        data={customData.data.filter(e => e.products_category === itemId)}
         renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()} // Gerar chave única para cada item
-        numColumns={2} // Número de colunas na grade
-        columnWrapperStyle={styles.columnWrapper} // Estilo para o wrapper das colunas
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
         contentContainerStyle={styles.contentContainer}
       />
     </View>
@@ -51,71 +118,118 @@ const ProductsByCategory = ({ route, navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#f2f2f2', // Cor de fundo da tela
-      height: "100%",
-    },
-    columnWrapper: {
-      justifyContent: 'space-between', // Espaçar os itens na linha
-      paddingHorizontal: 10, // Espaçamento lateral
-      paddingBottom: 0,
-      paddingTop: 20
-    },
-    contentContainer: {
-      paddingBottom: 80, // Espaço adicional no final da lista para evitar que o último item fique grudado no menu inferior
-    },
-    itemContainer: {
-      flex: 1,
-      height: 320,
-      alignItems: 'center',
-      borderRadius: 8,
-      marginBottom: 0, // Espaço entre as linhas
-      marginHorizontal: 5, // Espaço entre os itens na mesma linha
-    },
-    containerImage: {
-        width: "100%",
-        borderRadius: 20,
-        height: "85%",
-        position: 'relative',
-        backgroundColor: "#000000"
-    },
-    containerImageFavoriteButton: {
-        position: 'absolute', // Posiciona o botão no topo da imagem
-        top: 10, // Distância do topo
-        right: 10, // Distância da borda direita
-        width: 35, // Largura do botão
-        height: 35, // Altura do botão
-        backgroundColor: '#FFFFFF', // Cor de fundo do botão (exemplo)
-        borderRadius: 30, // Bordas arredondadas (exemplo)
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    image: {
-        width: "100%", // Ajuste conforme necessário
-        height: "100%", // Ajuste conforme necessário
-        borderRadius: 15
-      },
-    containerContent: {
-        width: "100%",
-        height: "15%",
-        flex: 1
-    },
-
-    textHeader:{
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    height: "100%",
+  },
+  textAboveCarousel: {
+    marginTop: 20, // Margem superior
+    alignItems: 'flex-start', // Alinha à esquerda
+    justifyContent: 'center',
+    paddingLeft: 20, // Adiciona espaçamento à esquerda para alinhar com os cards
+  },
+  carouselTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  carouselContainer: {
+    height: 200, // Altura do carousel
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop: 10, // Margem superior para o carousel
+  },
+  carouselItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carouselImage: {
+    height: 200,
+    width: '100%', // Ajusta a largura da imagem ao tamanho do item
+    borderRadius: 10,
+    resizeMode: 'cover', // Ajusta a imagem ao tamanho do container sem distorção
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  indicator: {
+    width: 6,
+    height: 6,
+    backgroundColor: '#fff',
+    borderRadius: 3,
+    margin: 3,
+  },
+  itemContainer: {
+    flex: 1,
+    height: 320,
+    alignItems: 'center',
+    borderRadius: 8,
+    marginBottom: 0,
+    marginHorizontal: 5,
+  },
+  containerImage: {
+    width: "100%",
+    borderRadius: 20,
+    height: "85%",
+    position: 'relative',
+    backgroundColor: "#000000",
+  },
+  containerImageFavoriteButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 35,
+    height: 35,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+  },
+  containerContent: {
+    width: "100%",
+    height: "15%",
+    flex: 1,
+  },
+  textHeader: {
     fontWeight: 'bold',
     marginTop: 5,
     fontSize: 14,
-    color: '#444'
-},
-
-    textContent:{
+    color: '#444',
+  },
+  textContent: {
     fontWeight: '400',
     marginTop: 2.5,
     fontSize: 12,
-    color: '#9C9C9C'
-    },
-
-  });
+    color: '#9C9C9C',
+  },
+  columnWrapper: {
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingBottom: 0,
+    paddingTop: 20,
+  },
+  contentContainer: {
+    paddingBottom: 80,
+  },
+  bottomImageContainer: {
+    marginTop: 0, // Margem superior da imagem abaixo do carousel
+  },
+  bottomImage: {
+    width: '100%',
+    height: 150, // Altura ajustada da imagem
+    resizeMode: 'cover', // Ajuste para manter a proporção
+  },
+});
 
 export default ProductsByCategory;
--- src/Screens/Category/category.js
+++ src/Screens/Category/category.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 import customData from "./category.json";
+
+const screenWidth = Dimensions.get('window').width;
 
 const CategoryScreen = ({ navigation }) => {
   // Função para renderizar cada item da lista
   const renderItem = ({ item }) => {
-    return(
+    return (
       <TouchableOpacity 
-      style={styles.itemContainer}
-      onPress={() => navigation.navigate('Produtos da Categoria', { itemId: item.products_category_id, itemName: item.category_name })}
+        style={styles.itemContainer}
+        onPress={() => navigation.navigate('Produtos da Categoria', { itemId: item.products_category_id, itemName: item.category_name })}
       >
-
         <View style={styles.innerContainer}>
-          <Image style={styles.image}         source={{
-          uri: item.category_image,
-        }} />
+          <Image style={styles.image} source={{ uri: item.category_image }} />
           <Text style={styles.text}>{item.category_name}</Text>
         </View>
-
       </TouchableOpacity>
     );
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      {/* Imagem no topo da tela */}
+      <Image
+        source={{ uri: 'https://raw.githubusercontent.com/ViniciusOkaeda/u-commerce/refs/heads/main/src/Assets/bfday_bgd.png' }} // URL da imagem
+        style={styles.responsiveImage}
+        resizeMode="contain" // Garantir que a imagem se ajuste corretamente
+      />
+
+      {/* FlatList que exibe os itens abaixo da imagem */}
       <FlatList
         data={customData.data}
         renderItem={renderItem}
@@ -32,7 +38,7 @@ const CategoryScreen = ({ navigation }) => {
         columnWrapperStyle={styles.columnWrapper} // Estilo para o wrapper das colunas
         contentContainerStyle={styles.contentContainer}
       />
-    </View>
+    </ScrollView>
   );
 };
 
@@ -40,13 +46,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f2f2f2', // Cor de fundo da tela
-    height: 400,
   },
   columnWrapper: {
     justifyContent: 'space-between', // Espaçar os itens na linha
     paddingHorizontal: 10, // Espaçamento lateral
-    paddingBottom: 0,
-    paddingTop: 90
+    paddingTop: 80
   },
   contentContainer: {
     paddingBottom: 80, // Espaço adicional no final da lista para evitar que o último item fique grudado no menu inferior
@@ -55,15 +59,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#fff',
-    borderRadius: 8,
-    marginBottom: 0, // Espaço entre as linhas
+    borderRadius: 5,
+    height: 110,
     padding: 10, // Padding ao redor dos itens
-    shadowColor: "#000000",
+    shadowColor: "#444444",
     shadowOffset: {
       width: 0,
       height: 1,
     },
-    shadowOpacity:  0.15,
+    shadowOpacity: 0.15,
     shadowRadius: 1.00,
     elevation: 1,
     marginHorizontal: 5, // Espaço entre os itens na mesma linha
@@ -72,18 +76,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center', // Centraliza o conteúdo verticalmente
     alignItems: 'center', // Centraliza o conteúdo horizontalmente
-    padding: 10, // Padding ao redor dos itens
+    padding: 5, // Padding ao redor dos itens
+  },
+  responsiveImage: {
+    marginTop: -40,
+    width: screenWidth, // Largura da imagem igual à largura da tela
+    height: 300, // Definindo altura fixa, ou pode ser ajustado proporcionalmente
+    resizeMode: 'contain', // Para garantir que a imagem mantenha sua proporção
   },
   text: {
     fontSize: 18,
-    marginBottom: 10,
+    marginTop: -50,
     fontWeight: 'bold',
     color: '#444'
   },
   image: {
     marginTop: -100,
     width: 150, // Ajuste conforme necessário
-    height: 150, // Ajuste conforme necessário
+    height: 180, // Ajuste conforme necessário
+    marginBottom: 40
   },
 });
 
